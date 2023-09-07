134d0a83683bce83d6546c30c4a23fc4749464da
aousdev <>
2023-09-07 20:04:00 +0000

    Refactor love quiz component in order to improve consitency: - Add reflection modal - Remove summary method

--- src/core/love-quiz-component/love-quiz.js
+++ src/core/love-quiz-component/love-quiz.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const loveQuiz = document.getElementById('love-quiz');
     const questionContainers = document.querySelectorAll('.question-container');
     const nextQuestionButtons = document.querySelectorAll('.next-question');
@@ -14,39 +14,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    loveQuiz.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const lastAnswer = document.getElementById('question4').value;
-        answers.push(lastAnswer);
-        console.log(answers);
-        // Display the summary of answers
-        displaySummary(answers);
+    const nextButtons = document.querySelectorAll('.next-question');
+    const reflectButtonContainer = document.getElementById('conditionalElement');
+
+    nextButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Get current question container
+            const currentContainer = this.closest('.question-container');
+            // Hide current question container
+            currentContainer.style.display = 'none';
+
+            // Get the next question container by its ID
+            const nextContainerId = currentContainer.getAttribute('id').replace(/\d+/, (match) => parseInt(match) + 1);
+            const nextContainer = document.getElementById(nextContainerId);
+
+            // Check if the next container is the last one
+            if (nextContainer === document.getElementById('question4-container')) {
+                reflectButtonContainer.style.display = 'flex';
+            }
+
+            // Show next question container
+            if (nextContainer) {
+                nextContainer.style.display = 'block';
+            }
+        });
+
+        // Open the reflection modal
+        const openModalButton = document.getElementById("open-reflection-modal");
+        const reflectionModal = new bootstrap.Modal(document.getElementById("reflectionModal"));
+
+        openModalButton.addEventListener("click", () => {
+            reflectionModal.show();
+        });
+
+        const reflectionButton = document.getElementById("submit-reflection");
+        reflectionButton.addEventListener('click', () => {
+            $("#modal-go-mission").modal('show')
+        });
     });
-});
+})
 
-
-document.getElementById('button-continue').addEventListener('click', function(){
+document.getElementById('button-continue').addEventListener('click', function () {
     window.location.href = '../mission-game-component/mission-game.html';
 })
-function displaySummary(answers) {
-    const mainContent = document.getElementById('main-content');
-    const summary = document.createElement('div');
-    summary.id = 'summary';
-    summary.style.minWidth = "400px";
-    summary.style.backgroundColor = 'rgb(240 255 255 / 50%)';
-    summary.style.borderRadius = "20px";
-    summary.innerHTML = '<h2>Summary</h2>';
-    answers.forEach((answer, index) => {
-        summary.innerHTML += `<p>Question ${index + 1}: ${answer}</p>`;
-    });
-    mainContent.innerHTML = '';
-    mainContent.appendChild(summary);
-
-    setTimeout(() =>{
-        $("#myModal").modal('show')
-    },1000)
-}
-
 
 
 
