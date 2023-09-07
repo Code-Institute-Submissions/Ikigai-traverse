040188d760cf3f5337f71ad5a578168d13f19aed
aousdev <>
2023-09-07 20:09:48 +0000

    Populate image-grid dynamically and implement drag-and-drop sorting: - Generate image items from IMAGES array - Configure Sortable.js for drag-and-drop on image-grid - Add event listeners for reflection and navigation modals

--- src/core/profession-puzzle-component/profession-puzzle.js
+++ src/core/profession-puzzle-component/profession-puzzle.js
@@ -1,5 +1,26 @@
+import { IMAGES } from "./images.js";
+
 document.addEventListener('DOMContentLoaded', function () {
     const imageGrid = document.getElementById('image-grid'); // Get the grid container element
+
+
+    IMAGES.forEach(imageSrc => {
+        const imageItem = document.createElement("div");
+        imageItem.classList.add("image-item");
+        imageItem.setAttribute("draggable", "true");
+
+        const img = document.createElement("img");
+        img.src = `../../assets/images/${imageSrc}`;
+        img.alt = imageSrc.split(".")[0]; // Extracting alt text from filename
+
+        imageItem.appendChild(img);
+        imageGrid.appendChild(imageItem);
+    });
+
+    // Open the reflection modal
+    const openModalButton = document.getElementById("open-reflection-modal");
+    const reflectionModal = new bootstrap.Modal(document.getElementById("reflectionModal"));
+    const reflectionButton = document.getElementById("submit-reflection");
 
     new Sortable(imageGrid, {
         group: "sorting",
@@ -9,11 +30,16 @@ document.addEventListener('DOMContentLoaded', function () {
         ghostClass: "ghost",
     });
 
-    // Open the reflection modal
-    const openModalButton = document.getElementById("open-reflection-modal");
-    const reflectionModal = new bootstrap.Modal(document.getElementById("reflectionModal"));
 
     openModalButton.addEventListener("click", () => {
         reflectionModal.show();
     });
+
+    reflectionButton.addEventListener('click', (event) => {
+        $("#modal-go-to-vocation").modal('show')
+    });
 });
+
+document.getElementById('button-continue').addEventListener('click', function(){
+    window.location.href = '../vocation-puzzle-component/vocation-puzzle.html';
+})
