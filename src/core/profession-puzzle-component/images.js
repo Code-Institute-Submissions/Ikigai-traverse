0db2a7d7f9446db44b1a566d752202f9cd612ae3
aousdev <>
2023-09-07 20:07:57 +0000

    Add images const to be used in the profession component

---
+++ src/core/profession-puzzle-component/images.js
@@ -0,0 +1,14 @@
+export const IMAGES = [
+    "building.jpg",
+    "cooking.jpg",
+    "crafting.jpg",
+    "design.jpg",
+    "e-book-layout-design-draft-graphic.jpg",
+    "fourniture_assembly+.jpg",
+    "management.jpg",
+    "music.jpg",
+    "painting.jpg",
+    "puzzle.jpg",
+    "sport_playing.jpg",
+    "voulanteers.jpg"
+];
