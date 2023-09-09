8394d4320293284ca79cda055581f5825d2e5657
aousdev <askriahlem100@gmail.com>
2023-09-10 01:58:29 +0200

    Replace JPG images with WebP images.

--- src/core/profession-puzzle-component/images.js
+++ src/core/profession-puzzle-component/images.js
@@ -1,14 +1,14 @@
 export const IMAGES = [
-    "building.jpg",
-    "cooking.jpg",
-    "crafting.jpg",
+    "building.webp",
+    "cooking.webp",
+    "crafting.webp",
     "design.jpg",
     "e-book-layout-design-draft-graphic.jpg",
-    "fourniture-assembly.jpg",
-    "management.jpg",
-    "music.jpg",
+    "fourniture-assembly.webp",
+    "management.webp",
+    "music.webp",
     "painting.jpg",
-    "puzzle.jpg",
-    "sport-playing.jpg",
-    "voulanteers.jpg"
+    "puzzle.webp",
+    "sport-playing.webp",
+    "voulanteers.webp"
 ];
